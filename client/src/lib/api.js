e51c1f8bcbe9d5b133216882ec0977236678e71a
Shane Raza <shaanali00478692@gmail.com>
2026-08-05 02:28:07 +0530

    Point frontend to deployed Render backend

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,10 +1,10 @@
 ﻿import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://kanban-app-3k08.onrender.com/api',
 });
 
-const TEMP_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjZhNmVmNzRhMmU3ZmVlYjUwMDViZmVmNyIsImlhdCI6MTc4NTczMjQzNywiZXhwIjoxNzg2MzM3MjM3fQ.MHKy1tH1jFR1gUrtRI55c03456BsyNhr5TPKni4VFWo';
+const TEMP_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjZhNmVmNzRhMmU3ZmVlYjUwMDViZmVmNyIsImlhdCI6MTc4NTg3NjgwMCwiZXhwIjoxNzg2NDgxNjAwfQ.KJCcoT9Jr367_9cQmAh32PUPkX46LTvJdmfQia_0dzI';
 
 api.interceptors.request.use((config) => {
   config.headers.Authorization = 'Bearer ' + TEMP_TOKEN;
--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,6 +1,6 @@
 ﻿import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000', {
+const socket = io('https://kanban-app-3k08.onrender.com', {
   autoConnect: false,
 });
 
